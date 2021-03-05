cc38761de283a9dfb5a67021c3d37f307b31d235
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-03-05 14:04:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Supernova
+
+A reference application for Nebula. 
+
+made with start.spring.io with maven/kotlin/jar chosse spring-web and spring-actualty
+
+
+## Build docker image
+`mvn spring-boot:build-image`
+
+Does not work behind VPN because...
+
---
+++ src/main/kotlin/no/skatteetaten/nebula/demo/supernova/SupernovaApplication.kt
@@ -0,0 +1,24 @@
+package no.skatteetaten.nebula.demo.supernova
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class SupernovaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SupernovaApplication>(*args)
+}
+
+
+@RestController
+@RequestMapping("/")
+class SuperNovaController() {
+
+    @GetMapping
+    fun index() = "Hello Nebula"
+
+}
