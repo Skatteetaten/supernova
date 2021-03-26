cdc17e6749d54e5505f7c97a39b6496855b01430
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-03-26 12:47:49 +0100

    bump version

--- src/main/kotlin/no/skatteetaten/nebula/demo/supernova/SupernovaApplication.kt
+++ src/main/kotlin/no/skatteetaten/nebula/demo/supernova/SupernovaApplication.kt
@@ -13,12 +13,11 @@ fun main(args: Array<String>) {
 	runApplication<SupernovaApplication>(*args)
 }
 
-
 @RestController
 @RequestMapping("/")
 class SuperNovaController() {
 
     @GetMapping
-    fun index() = "Hello Nebula DEV 3"
+    fun index() = "Hello Nebula DEV 5"
 
 }
