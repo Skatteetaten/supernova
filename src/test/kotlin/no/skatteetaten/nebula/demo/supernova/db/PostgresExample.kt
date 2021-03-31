7ae405f8b8317dd86945b4e268397ecb170a4c53
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-03-31 10:32:46 +0200

    fixed postgres test

--- src/test/kotlin/no/skatteetaten/nebula/demo/supernova/db/PostgresExample.kt
+++ src/test/kotlin/no/skatteetaten/nebula/demo/supernova/db/PostgresExample.kt
@@ -1,14 +1,19 @@
 package no.skatteetaten.nebula.demo.supernova.db
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import org.skyscreamer.jsonassert.JSONAssert
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForObject
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.util.AssertionErrors.assertEquals
 
 @ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,19 +23,20 @@ class AwesomeTestcontainersApplicationTests() {
     var randomServerPort = 0
 
     var restTemplate = TestRestTemplate()
-    var headers: HttpHeaders = HttpHeaders()
 
     @Test
     fun returnsListOfPersons() {
-        val entity = HttpEntity<String>(null, headers)
 
-        val response = restTemplate.exchange(
-                createURLWithPort("/persons"),
-                HttpMethod.GET, entity, String::class.java)
+        val response: ArrayNode = restTemplate.getForObject(createURLWithPort("/persons")) ?: throw Exception("Failed getting persons")
 
-        val expected = "[{name:\"Marcus Eisele\"},{name:\"John Doe\"}]"
+        val expected = """[{"name":"Marcus Eisele"},{"name":"John Doe"}]"""
 
-        JSONAssert.assertEquals(expected, response.body, false)
+        val expectedJson: ArrayNode = ObjectMapper().readValue(expected)
+
+        assert(response.size() == 2)
+        assertEquals("Should be equal", response, expectedJson)
+
+
     }
 
     private fun createURLWithPort(uri: String): String {
